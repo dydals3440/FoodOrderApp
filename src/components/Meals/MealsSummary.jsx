c91b12509e4a79c8e1bb5047f39d1f2b1ba3a557
김용민 <dydals3440@gmail.com>
2023-07-24 21:47:10 +0900

    feat: Meals Component

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,21 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>맛있는 음식, 배달해드려요~</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+        voluptas inventore veritatis sint a possimus eaque repudiandae illum
+        sunt laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas est
+        officia odio. Quo, libero sit accusamus nobis assumenda consectetur
+        error?
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
