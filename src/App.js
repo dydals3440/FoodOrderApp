ced58c3e81ac0d258e41527580f9fd484dc27bc6
김용민 <dydals3440@gmail.com>
2023-07-24 21:18:45 +0900

    feat: Cart component 작업 + 스타일링

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Header } from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <h2>Let's get started!</h2>
-    </div>
+    </>
   );
 }
 
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 export const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       {/* -가 들어간 css 파일명이므로, 아래와 같이 해야함 .은 사용x */}
       <div className={classes['main-image']}>
