6c9860f92948bd42ea6f7f1c819fc452757f3232
김용민 <dydals3440@gmail.com>
2023-07-24 20:55:53 +0900

    first commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      {/* -가 들어간 css 파일명이므로, 아래와 같이 해야함 .은 사용x */}
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='meals jpg' />
+      </div>
+    </Fragment>
+  );
+};
